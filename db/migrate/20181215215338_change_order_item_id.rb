515ef9bddf20e55450dc7b414342a1f29bd12260
Anton Andrushin <andrushin.anton@gmail.com>
2018-12-16 00:21:09 +0200

    wip

--- db/migrate/20181215215338_change_order_item_id.rb
+++ db/migrate/20181215215338_change_order_item_id.rb
@@ -1,5 +1,5 @@
 class ChangeOrderItemId < ActiveRecord::Migration[5.2]
   def change
-    change_column :order_items, :order_id, :string
+    # change_column :order_items, :order_id, :string
   end
 end
